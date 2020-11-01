e3211d968f97ededff47956842195a2ec7568596
artificIn <djgmd1021@gmail.com>
2020-11-01 17:49:32 +0900

    Init project

---
+++ 90s/90s/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  90s
+//
+//  Created by 성다연 on 2020/11/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 90s/Pods/Target Support Files/Pods-90s/Pods-90s-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_90s : NSObject
+@end
+@implementation PodsDummy_Pods_90s
+@end
