cc421b603190155cdd23117abaf9d10e4692aaf4
artificIn <djgmd1021@gmail.com>
2021-02-23 01:15:48 +0900

    FilmListVC - tableview Section Setting Complete

--- 90s/90s/View/Film/FilmListVC/FilmListVC.swift
+++ 90s/90s/View/Film/FilmListVC/FilmListVC.swift
@@ -28,7 +28,7 @@ extension FilmListSectionData : SectionModelType {
 
 /// 필름 리스트
 class FilmListVC: UIViewController {
-    private var tableView = UITableView(frame: .zero, style: .plain)
+    private var tableView = UITableView(frame: .zero, style: .grouped)
     
     private let viewModel = FilmsViewModel()
     private var disposeBag = DisposeBag()
@@ -39,8 +39,7 @@ class FilmListVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTableView()
-        setSection()
-//        bindViewModel()
+        setUpTableViewSection()
     }
 }
 
@@ -51,45 +50,73 @@ extension FilmListVC : UITableViewDelegate {
         tableView.delegate = self
         tableView.showsVerticalScrollIndicator = false
         tableView.separatorStyle = .none
+        tableView.backgroundColor = .white
+        
         tableView.register(FilmListTableViewCell.self, forCellReuseIdentifier: FilmListTableViewCell.FilmListCellId)
+        tableView.register(FilmListSectionHeaderCell.self, forHeaderFooterViewReuseIdentifier: FilmListSectionHeaderCell.FilmListSectionHeaderCellID)
         
         tableView.snp.makeConstraints {
             $0.edges.equalTo(view)
         }
     }
     
-    private func setSection(){
+    private func setUpTableViewSection(){
         section = [
-            FilmListSectionData(header: "사진을 추가해주세요!", items: viewModel.getStateData(state: .adding)),
-            FilmListSectionData(header: "지금 인화하고 있어요", items: viewModel.getStateData(state: .printing)),
-            FilmListSectionData(header: "", items:
-                viewModel.getStateData(state: .complete))
+            FilmListSectionData(header: "", items: viewModel.getStateData(state: .adding)),
+            FilmListSectionData(header: "", items: viewModel.getStateData(state: .printing)),
+            FilmListSectionData(header: "", items: viewModel.getStateData(state: .complete))
         ]
         
         dataSource = RxTableViewSectionedReloadDataSource<FilmListSectionData> (configureCell: { dataSource, tableView, indexPath, item in
             let cell = tableView.dequeueReusableCell(withIdentifier: FilmListTableViewCell.FilmListCellId) as! FilmListTableViewCell
             cell.bindViewModel(film: item)
-            
             return cell
         }, titleForHeaderInSection: { dataSource, sectionIndex in
             return dataSource[sectionIndex].header
         })
         
+        
         Observable.just(section)
             .bind(to: tableView.rx.items(dataSource: dataSource!))
             .disposed(by: disposeBag)
     }
+}
+
+extension FilmListVC {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: FilmListSectionHeaderCell.FilmListSectionHeaderCellID) as! FilmListSectionHeaderCell
+        
+        header.backgroundView = UIView(frame: header.bounds)
+        header.backgroundView?.backgroundColor = .white
+        
+        switch section {
+        case 0:
+            header.bindViewModel(text: "사진을 추가해보세요!")
+            header.bindBlackView(hidden: true)
+        case 1:
+            header.bindViewModel(text: "지금 인화하고 있어요")
+            header.bindBlackView(hidden: true)
+        case 2:
+            header.bindViewModel(text: "")
+            header.bindBlackView(hidden: false)
+        default:
+            header.bindViewModel(text: "")
+        }
+        
+        return header
+    }
     
-    private func bindViewModel(){
-        viewModel.FilmObservable
-            .map { $0.filter { $0.id != "0" }} // 필름 만들기 제외
-            .bind(to: tableView.rx.items) { tableView, index, element in
-                let cell = tableView.dequeueReusableCell(withIdentifier: FilmListTableViewCell.FilmListCellId) as! FilmListTableViewCell
-                cell.bindViewModel(film: element)
-                
-                return cell
-            }
-            .disposed(by: disposeBag)
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        switch section {
+        case 0:
+            return 50.0
+        case 1:
+            return 70.0
+        case 2:
+            return 40.0
+        default :
+            return 40.0
+        }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
