8794658e25b8acd58da8a6f5440e18fcf315a894
woongs <ccw1021@naver.com>
2021-03-06 14:47:20 +0900

    Ad StickerPackViewModel

--- 90s/90s/ViewModel/Stickers/StickerPackViewModel.swift
+++ 90s/90s/ViewModel/Stickers/StickerPackViewModel.swift
@@ -2,52 +2,55 @@
 //  StickerPackViewModel.swift
 //  90s
 //
-//  Created by woong on 2021/03/05.
+//  Created by woong on 2021/03/06.
 //
 
+import Foundation
 import RxSwift
 import RxRelay
 
-class StickerPackListViewModel: ViewModelType {
-    private(set) var dependency: Dependency
-    private(set) var input = Input()
-    private(set) var output = Output()
-    private(set) var disposeBag = DisposeBag()
-
+class StickerPackViewModel: ViewModelType {
+    var dependency: Dependency
+    var input = Input()
+    var output = Output()
+    var disposeBag = DisposeBag()
+    
     required init(dependency: Dependency) {
         self.dependency = dependency
+        output.packName.accept(dependency.stickerPack.name)
+        output.stickers.accept(dependency.stickerPack.stickers)
         
-        input.selectedCategory
-            .bind(to: output.currentCategory)
+        input.fetchStickers
+            .map { $0.stickers }
+            .bind(to: output.stickers)
             .disposed(by: disposeBag)
         
-        input.selectedCategory
-            .map { dependency.stickerFactory.stickerPackList(of: $0) }
-            .bind(to: output.stickerPackList)
+        input.fetchStickers
+            .map { $0.name }
+            .bind(to: output.packName)
             .disposed(by: disposeBag)
         
-        input.selectedStickerPack
-            .subscribe(onNext: {[unowned self] index in
-                let packs = dependency.stickerFactory.stickerPackList(of: self.output.currentCategory.value)
-                self.output.showStickersOfPack.onNext(packs[index])
+        input.addSticker
+            .map { [unowned self] in self.output.stickers.value[$0] }
+            .subscribe(onNext: { sticker in
+                print(sticker)
             })
             .disposed(by: disposeBag)
     }
 }
 
-extension StickerPackListViewModel {
+extension StickerPackViewModel {
     struct Dependency {
-        var stickerFactory = StickerFactory()
+        var stickerPack: StickerPack
     }
-
+    
     struct Input {
-        var selectedCategory = BehaviorSubject<StickerPackCategory>(value: .basic)
-        var selectedStickerPack = PublishSubject<Int>()
+        var fetchStickers = PublishSubject<StickerPack>()
+        var addSticker = PublishSubject<Int>()
     }
-
+    
     struct Output {
-        var currentCategory = BehaviorRelay<StickerPackCategory>(value: .basic)
-        var stickerPackList = BehaviorRelay<[StickerPack]>(value: [])
-        var showStickersOfPack = PublishSubject<StickerPack>()
+        var stickers = BehaviorRelay<[Sticker]>(value: [])
+        var packName = BehaviorRelay<String>(value: "스티커팩이름")
     }
 }
