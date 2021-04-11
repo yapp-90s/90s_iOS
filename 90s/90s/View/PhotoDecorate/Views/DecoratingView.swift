5b34573e0adc4148de2370ed04d91ae3d90d5050
woongs <ccw1021@naver.com>
2021-04-11 12:23:07 +0900

    Remove DecoratingView
    DecoratingView에서 Decorator를 가지고 있어서 컨트롤 하는 게 어색하다고 판단, 제거하고 뷰컨으로 옮김

--- 90s/90s/View/PhotoDecorate/Views/DecoratingView.swift
+++ 90s/90s/View/PhotoDecorate/Views/DecoratingView.swift
@@ -12,36 +12,7 @@ class DecoratingView: UIView {
     
     // MARK: Properties
     
-    private var decorator = StickerDecorator()
     
-    // MARK: - Methods
     
-    func attachStickerView(_ sticker: ResizableStickerView, at position: CGPoint? = nil) {
-        addSubview(sticker)
-        sticker.center = position ?? center
-        addMovingGesture(sticker)
-        addResizingGesture(sticker)
-    }
     
-    // MARK: Private
-    
-    private func addMovingGesture(_ sticker: ResizableStickerView) {
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(moveSticker(_:)))
-        
-        sticker.addGestureRecognizer(panGesture)
-    }
-    
-    private func addResizingGesture(_ sticker: ResizableStickerView) {
-        sticker.resizeHandler = { [weak self] gesture, sticker in
-            self?.panGestureResizing(sticker, with: gesture)
-        }
-    }
-    
-    @objc private func moveSticker(_ gesture: UIPanGestureRecognizer) {
-        decorator.moveSticker(with: gesture, in: self)
-    }
-    
-    private func panGestureResizing(_ sticker: ResizableStickerView, with gesture: UIPanGestureRecognizer) {
-        decorator.resizingSticker(sticker, with: gesture, in: self)
-    }
 }
