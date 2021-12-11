7e6f471afe4172dc3d6fae56423ca1673604aea8
woongs <ccw1021@naver.com>
2021-12-11 13:22:58 +0900

    LoginViewModel > 애플 로그인 관련 로직 추가

--- 90s/90s/ViewModel/Login/LoginViewModel.swift
+++ 90s/90s/ViewModel/Login/LoginViewModel.swift
@@ -8,12 +8,16 @@
 import Foundation
 import RxSwift
 
-final class LoginViewModel: ViewModelType {
+final class LoginViewModel: ViewModelType, AppleLoginControllerDelegate {
     
     private(set) var dependency: Dependency
     private(set) var input = Input()
     private(set) var output: Output
     private(set) var disposeBag = DisposeBag()
+    
+    private var loginService: LoginService {
+        return self.dependency.loginService
+    }
     
     required init(dependency: Dependency) {
         self.dependency = dependency
@@ -29,14 +33,18 @@ final class LoginViewModel: ViewModelType {
                 self?.requestLogin(type: loginType)
             })
             .disposed(by: self.disposeBag)
+        
+        self.dependency.appleLoginController.delegate = self
+    }
+    
+    public func setupAppleLoginPresentaion(_ presentation: AppleLoginPresentable) {
+        self.dependency.appleLoginController.viewController = presentation
     }
     
     private func requestLogin(type loginType: LoginType) {
-        let service = self.dependency.loginService
-        
         switch loginType {
         case .kakao:
-            service.requestLogin(.kakao)
+            self.loginService.requestKakaoLogin()
                 .subscribe(onNext: { [weak self] loginOAuth in
                     guard let self = self else { return }
                     if let _ = loginOAuth {
@@ -54,12 +62,28 @@ final class LoginViewModel: ViewModelType {
         case .google:
             return
         case .apple:
-            return
+            self.dependency.appleLoginController.handleAuthorizationAppleIDButtonPress()
         }
     }
     
-    private func requestKakaoEmail() {
-        
+    func appleLoginController(_ controller: AppleLoginController, didLoginSucceedWith userInfo: AppleLoginController.CredentialUserInfo) {
+        if let email = userInfo.email {
+            self.loginService.requestAppleLogin(email: email)
+                .subscribe(onNext: { [weak self] loginOAuth in
+                    if let _ = loginOAuth {
+                        // 로그인
+                    } else {
+                        self?.output.signUpNeeded.onNext(())
+                    }
+                })
+                .disposed(by: self.disposeBag)
+        } else {
+            // TODO: handle Login Fail
+        }
+    }
+    
+    func appleLoginController(_ controller: AppleLoginController, didLoginFailWith error: Error) {
+        // TODO: handle Login Fail
     }
 }
 
@@ -67,6 +91,7 @@ extension LoginViewModel {
     
     struct Dependency {
         let loginService: LoginService = LoginService()
+        let appleLoginController: AppleLoginController = AppleLoginController()
     }
     
     struct Input {
