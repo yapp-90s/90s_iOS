c4411fd3335ebb8fa0a0a82d146c7856b1b2e1eb
woongs <ccw1021@naver.com>
2021-12-11 17:19:45 +0900

    Pod update

--- 90s/90s/Entry/AppDelegate.swift
+++ 90s/90s/Entry/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        RxKakaoSDKCommon.initSDK(appKey: "9e142b2bf8f1bb10976efdb1322c0c61")
+        RxKakaoSDKCommon.RxKakaoSDK.initSDK(appKey: "9e142b2bf8f1bb10976efdb1322c0c61")
         return true
     }
 
