769b592d2b5ec3e1b99189d2c0e70b66b04ceee2
woongs <ccw1021@naver.com>
2021-12-11 16:50:44 +0900

    로그인 서비스 수정

--- 90s/90s/Service/Login/LoginService.swift
+++ 90s/90s/Service/Login/LoginService.swift
@@ -13,10 +13,11 @@ class LoginService {
     
     typealias CertificationNumber = String
     
-    let userManager: UserManager
-    let kakaoSDKService: KakaoSDKService
-    let provider = MoyaProvider<LoginAPI>()
-    
+    private let userManager: UserManager
+    private let kakaoSDKService: KakaoSDKService
+    private let provider = MoyaProvider<LoginAPI>()
+    private var loginType: LoginType?
+
     init(userManager: UserManager = .shared, kakaoSDKService: KakaoSDKService = KakaoSDKServiceImp()) {
         self.userManager = userManager
         self.kakaoSDKService = kakaoSDKService
@@ -25,6 +26,7 @@ class LoginService {
     // MARK: - Public
     
     public func requestKakaoLogin() -> Observable<LoginOAuthToken?> {
+        self.loginType = .kakao
         return self.kakaoSDKService.requestKakaoLogin()
             .flatMap { [weak self] _ -> Single<String> in
                 guard let self = self else { return .error(APIError.networkFail) }
@@ -35,7 +37,7 @@ class LoginService {
             }
             .flatMap { [weak self] email -> Single<LoginOAuthToken?> in
                 guard let self = self else { return .error(APIError.networkFail) }
-                return self.requestLogin(type: .kakao, email: email)
+                return self.requestLogin(type: .kakao, email: email, phoneNumber: "")
             }
             .catchError({ error -> Observable<LoginOAuthToken?> in
                 return .error(error)
@@ -44,8 +46,19 @@ class LoginService {
     }
     
     public func requestAppleLogin(email: String) -> Observable<LoginOAuthToken?> {
+        self.loginType = .apple
         self.userManager.saveUserEmail(email)
-        return self.requestLogin(type: .apple, email: email).asObservable()
+        return self.requestLogin(type: .apple, email: email, phoneNumber: "").asObservable()
+    }
+    
+    public func requestSignUp(phoneNumber: String) -> Observable<LoginOAuthToken?> {
+        guard let loginType = self.loginType,
+              let email = userManager.userEmail
+        else {
+            return .error(APIError.NotSocialLogin)
+        }
+        
+        return self.requestLogin(type: loginType, email: email, phoneNumber: phoneNumber).asObservable()
     }
     
     public func requestCheckPhoneNumber(_ phoneNumber: String) -> Single<CertificationNumber> {
@@ -54,13 +67,17 @@ class LoginService {
             .map { $0.num }
     }
     
+    public func saveUserToken(_ token: String) {
+        self.userManager.saveToken(token)
+    }
+    
     // MARK: - Private
     
-    private func requestLogin(type: LoginType, email: String) -> Single<LoginOAuthToken?> {
-        return self.provider.rx.request(.login(type: .kakao, email: email, phoneNumber: ""))
+    private func requestLogin(type: LoginType, email: String, phoneNumber: String = "") -> Single<LoginOAuthToken?> {
+        return self.provider.rx.request(.login(type: .kakao, email: email, phoneNumber: phoneNumber))
             .flatMap({ response in
                 if let loginResponse = try? response.map(LoginResponse.self) {
-                    return .just(LoginOAuthToken(loginResponse.jwt, ""))
+                    return .just(LoginOAuthToken(loginResponse.jwt, email))
                 } else if let emailNotExistResponse = try? response.map(ErrorResponse.self), emailNotExistResponse.status == 400 {
                     return .just(nil)
                 } else {
