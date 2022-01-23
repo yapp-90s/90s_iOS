29f885cd2515f48321d9fffa4b5818453db6a613
kimjinu <kimjinu5555@gmail.com>
2022-01-23 19:33:29 +0900

    [Clean] Template PortraBlack 코드 정리

--- 90s/90s/View/Album/Template/Portra Black/TemplatePortraBlack.swift
+++ 90s/90s/View/Album/Template/Portra Black/TemplatePortraBlack.swift
@@ -11,18 +11,7 @@ import SnapKit
 
 final class TemplatePortraBlack: UIView, TemplateView {
     
-    let scale = UIScreen.main.bounds.width / 323
-    
-    var currentIndex: Int = -1
-//    enum Kind {
-//        case portraBlack
-//        case portraWhite
-//        case moodyPaper
-//        case grass
-//        case polaroid
-//        case gradient
-//    }: TemplateImageViewDelegate?
-    
+    // MARK: - UI Component
     lazy var imageView1: PortraBlackTemplateImageView = {
         let imageView = PortraBlackTemplateImageView()
         imageView.tag = 0
@@ -50,6 +39,18 @@ final class TemplatePortraBlack: UIView, TemplateView {
         return imageView
     }()
     
+    // MARK: - Property
+    let scale = UIScreen.main.bounds.width / 323
+    
+    var isEditing: Bool = false {
+        didSet {
+            setEditing(isEditing)
+        }
+    }
+    weak var delegate: TemplateViewDelegate?
+    var currentIndex: Int = -1
+    
+    // MARK: - Init
     init() {
         super.init(frame: .zero)
         
@@ -60,6 +61,7 @@ final class TemplatePortraBlack: UIView, TemplateView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // MARK: - Setup Method
     private func setLayout() {
         backgroundColor = .black
         imageView1.snp.makeConstraints {
@@ -80,51 +82,49 @@ final class TemplatePortraBlack: UIView, TemplateView {
             $0.left.equalToSuperview().offset(46 * scale)
         }
     }
+    
+    private func setEditing(_ isEditing: Bool) {
+        imageView1.isUserInteractionEnabled = isEditing
+        imageView2.isUserInteractionEnabled = isEditing
+        imageView3.isUserInteractionEnabled = isEditing
+        
+        if isEditing {
+            if imageView1.image == nil {
+                imageView1.image = .init(named: "Icon_Add_Photo")
+                imageView1.contentMode = .center
+            }
+            if imageView2.image == nil {
+                imageView2.image = .init(named: "Icon_Add_Photo")
+                imageView2.contentMode = .center
+            }
+            if imageView3.image == nil {
+                imageView3.image = .init(named: "Icon_Add_Photo")
+                imageView3.contentMode = .center
+            }
+        } else {
+            imageView1.contentMode = .scaleAspectFit
+            imageView2.contentMode = .scaleAspectFit
+            imageView3.contentMode = .scaleAspectFit
+        }
+    }
+    
+    // MARK: - Interface
+    func bind(page: Page) {
+        for index in 0..<page.imagesURL.count {
+            if index == 0 {
+                imageView1.imageURL = page.imagesURL[index]
+            } else if index == 1 {
+                imageView2.imageURL = page.imagesURL[index]
+            } else if index == 2 {
+                imageView3.imageURL = page.imagesURL[index]
+            }
+        }
+    }
 }
 
 extension TemplatePortraBlack: TemplateImageViewDelegate {
     func tapped(_ index: Int) {
         currentIndex = index
-        let imagePicker = UIImagePickerController()
-        imagePicker.sourceType = .photoLibrary
-        imagePicker.allowsEditing = true
-        imagePicker.delegate = self
-        DispatchQueue.main.async {
-            UIApplication.shared.keyWindow?.rootViewController?.present(imagePicker, animated: true, completion: nil)
-        }
-    }
-}
-
-extension TemplatePortraBlack: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        var image: UIImage? = nil
-        
-        if let possibleImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
-            image = possibleImage
-        } else if let possibleImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            image = possibleImage
-        }
-        
-        if currentIndex == 0 {
-            imageView1.image = image
-        } else if currentIndex == 1 {
-            imageView2.image = image
-        } else {
-            imageView3.image = image
-        }
-        
-//        addImageButton.snp.removeConstraints()
-//        addImageButton.snp.remakeConstraints {
-//            $0.width.height.equalTo(80 * AppService.shared.layoutScale)
-//            $0.top.equalTo(titleLabel.snp.bottom).offset(16 * AppService.shared.layoutScale)
-//            $0.left.equalToSuperview().offset(32 * AppService.shared.layoutScale)
-//        }
-//        addImageButton.setImage(image, for: .normal)
-//        if let image = image {
-//            imageView.image = image
-//        }
-        DispatchQueue.main.async {
-            picker.dismiss(animated: true, completion: nil)
-        }
+        delegate?.didTapPhoto(index: index)
     }
 }
